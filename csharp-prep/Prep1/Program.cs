0653dd698e5e15d092ed96967802fda1ec634292
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-04-20 14:18:43 -0600

    Completed Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,17 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        string first_Name;
+        string last_Name;
+
         Console.WriteLine("Hello Prep1 World!");
+    
+        Console.Write("What is your first name? ");
+        first_Name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        last_Name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_Name}, {first_Name} {last_Name}.");
     }
 }
