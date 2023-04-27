94b16314d5cb1297fe7eeb9260205fc4d4a52bd5
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-04-26 23:17:39 -0600

    Prep 1 And 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        int lastdigit;
+        string grade_opt;
+
+        grade = 0;
+        grade_opt = "";
+        Console.Write("What is your grade precentage? ");
+        grade = int.Parse(Console.ReadLine());
+        lastdigit = (grade % 10);
+        
+        if (grade < 97 || grade > 60)
+        {
+            if (lastdigit >= 7)
+            {
+            grade_opt = "+";
+            }
+            else if (lastdigit < 3)
+            {
+            grade_opt = "-";
+            }
+        }
+        
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"You got an 'A{grade_opt}' ");
+            Console.WriteLine("You Passed the Class");
+            
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine($"You got an 'B{grade_opt}' ");
+            Console.WriteLine("You Passed the Class");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine($"You got an 'C{grade_opt}' ");
+            Console.WriteLine("You Passed the Class");
+
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine($"You got an 'D{grade_opt}' ");
+            Console.WriteLine("You did not pass the class try to get your grade up to at least a 70% ");
+        
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("You got an 'F' ");
+            Console.WriteLine("You did not pass the class try to get your grade up to at least a 70% ");
+        }
+        else
+            Console.WriteLine("Invalid entry");
     }
 }
