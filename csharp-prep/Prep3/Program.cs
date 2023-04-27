2820c27774a8e47519577deba79c338e643a15b5
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-04-27 14:29:26 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int magicNumber = generator.Next(1,101);
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumber) 
+        {
+            Console.Write("Guess a number: ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > magicNumber) {
+                Console.WriteLine("Guess Lower!");
+            }
+            else if ( userGuess < magicNumber) {
+                Console.WriteLine("Guess Higher!");
+            }
+        }
+        Console.WriteLine("You are correct!");
     }
 }
